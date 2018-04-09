f26a5d32e278491ead199d2e8449a58a7cd5ebff
Akash <akash.darknight@gmail.com>
2018-04-09 10:03:00 +0530

    Script

--- Happy-House.py
+++ Happy-House.py
@@ -27,3 +27,34 @@ np.set_printoptions(threshold=np.nan)
 FRmodel = faceRecoModel(input_shape=(3, 96, 96))
 In [3]:
 print("Total Params:", FRmodel.count_params())
+
+
+def triplet_loss(y_true, y_pred, alpha=0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+
+    Returns:
+    loss -- real number, value of the loss
+    """
+
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    ### START CODE HERE ### (≈ 4 lines)
+    # Step 1: Compute the (encoding) distance between the anchor and the positive
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)))
+    # Step 2: Compute the (encoding) distance between the anchor and the negative
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)))
+    # Step 3: subtract the two previous distances and add alpha.
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
+    loss = tf.maximum(tf.reduce_mean(basic_loss), 0.0)
+    ### END CODE HERE ###
+
+    return loss
