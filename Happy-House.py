7032fa40a87276e5230a5e7682f058f966293cc1
Akash <akash.darknight@gmail.com>
2018-05-05 10:03:00 +0530

    Script

--- Happy-House.py
+++ Happy-House.py
@@ -128,3 +128,28 @@ def verify(image_path, identity, database, model):
 verify("images/camera_0.jpg", "younes", database, FRmodel)
 
 verify("images/camera_2.jpg", "kian", database, FRmodel)
+
+
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the happy house by finding who is the person on the image_path image.
+
+    Arguments:
+    image_path -- path to an image
+    database -- database containing image encodings along with the name of the person on the image
+    model -- your Inception model instance in Keras
+
+    Returns:
+    min_dist -- the minimum distance between image_path encoding and the encodings from the database
+    identity -- string, the name prediction for the person on image_path
+    """
+
+    ### START CODE HERE ###
+
+    ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
+    encoding = img_to_encoding(image_path, model)
+
+    ## Step 2: Find the closest encoding ##
+
+    # Initialize "min_dist" to a large value, say 100 (≈1 line)
+    min_dist = 100
